11ffcdebed80a003c03a6b492b5046f15b1ce7db
YounesKBE <122362381+younesx207@users.noreply.github.com>
2024-01-03 14:56:55 +0100

    first_c

---
+++ python_testing/tests/test_101.py
@@ -0,0 +1,13 @@
+def test_101 ():
+    print("\n100+1")
+    assert 100+1 == 101
+    print("99+2")
+    assert 99+2 == 101
+
+def test_102 ():
+    print("\n100+2")
+    assert 100+2 == 102
+
+def test_103 ():
+    print("\n100+3")
+    assert 100+3 == 103
---
+++ python_testing/tests/test_get_review.py
@@ -0,0 +1,13 @@
+from RestaurantReviews import RestaurantReviews, ReviewDoesNotExists
+import pytest
+
+def test_get_valid_review () :
+    rr = RestaurantReviews()
+    rr.reviews["Cafe Mocha"] = {'comment':"Great coffee and pastries", 'rating':5}
+    assert rr.get_review("Cafe Mocha") == {'comment':"Great coffee and pastries", 'rating':5}
+
+def test_get_invalid_review () :
+    with pytest.raises(ReviewDoesNotExists) as e: #review does not exists
+        rr = RestaurantReviews()
+        rr.get_review("Cafe Mocha")
+    assert str(e.value) == "Cafe Mocha does not have review"
